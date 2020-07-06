b3987ae19ada168eb1d5e0d66d0a75c9b0b549d4
Shinichi Sato <hyotang.karakoma@gmail.com>
2020-07-06 18:26:15 +0900

    Add: Spec for MAKE-SLURPER

--- spec/slurp.lisp
+++ spec/slurp.lisp
@@ -3,3 +3,70 @@
 (in-package :slurp.spec)
 (setup :slurp)
 
+(requirements-about MAKE-SLURPER :doc-type function)
+
+;;;; Description:
+
+#+syntax
+(MAKE-SLURPER stream reader) ; => result
+
+; MAKE-SLURPER return slurper function.
+#?(MAKE-SLURPER (MAKE-STRING-INPUT-STREAM "dummy") #'READ-CHAR) :be-the FUNCTION
+
+; Slurper slurps content of STREAM with READER.
+#?(funcall (make-slurper (make-string-input-stream "dummy") #'read-line))
+=> "dummy"
+,:test equal
+
+; Slurper returns NIL when get END-OF-FILE.
+#?(let ((slurper (make-slurper (make-string-input-stream "dummy") #'read-char)))
+    (loop :repeat 7 :collect (funcall slurper)))
+=> (#\d #\u #\m #\m #\y nil nil)
+,:test equal
+
+;;;; Arguments and Values:
+
+; stream := Open input stream, otherwise signals condition depending on implementation.
+#?(MAKE-SLURPER "not-stream" #'READ-CHAR) :signals CONDITION
+
+; Not input.
+#?(make-slurper (make-string-output-stream) 'read-char)
+:signals stream-error
+
+; Not open.
+#?(make-slurper (let ((stream (make-string-input-stream "dummy")))
+                  (close stream)
+                  stream)
+                'read-char)
+:signals stream-error
+
+; Stream will be closed when get end-of-file.
+#?(let* ((stream (make-string-input-stream ""))
+         (slurper (make-slurper stream #'read)))
+    (values (open-stream-p stream)
+            (funcall slurper)
+            (open-stream-p stream)))
+:values (t nil nil)
+
+; reader := (or symbol (function (stream) *)), otherwise signals condition depending on implementation.
+#?(MAKE-SLURPER (MAKE-STRING-INPUT-STREAM "dummy") "not-function-designator") :signals CONDITION
+
+; result := (function nil *)
+
+;;;; Affected By:
+; Status of stream.
+
+;;;; Side-Effects:
+; Consume stream contents.
+
+;;;; Notes:
+; Slurper is GCed, stream will be closed.
+#?(let ((stream (make-string-input-stream "")))
+    (make-slurper stream #'read)
+    (trivial-garbage:gc)
+    (open-stream-p stream))
+=> NIL
+
+;;;; Exceptional-Situations:
+; When specified READER is not function as (function (stream) t), the behavior is unspecified.
+#?(make-slurper (make-string-input-stream "") #'car) => unspecified
--- spec/slurp.test.asd
+++ spec/slurp.test.asd
@@ -2,7 +2,7 @@
 (in-package :asdf)
 (defsystem "slurp.test"
   :version
-  "0.0.0"
+  "0.1.0"
   :depends-on
   (:jingoh "slurp")
   :components
